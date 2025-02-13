142e4091858bf57b5653e8634326a1f4cf493a53
voldemar <voldemar22864@gmail.com>
2025-02-14 00:33:18 +0300

    edit: minor improvements to html

--- frontend/src/components/posts_landing/post_card/PostCard.js
+++ frontend/src/components/posts_landing/post_card/PostCard.js
@@ -3,32 +3,6 @@ import React from "react";
 import { useLocation } from "react-router-dom";
 
 function PostCard({ key, card}) {
-    const pathName = useLocation();
-
-    const hoursNumber = Math.floor(card.duration / 60);
-    const minutesNumber = card.duration % 60;
-    let hours;
-    let minutes;
-
-    if (hoursNumber === 1) {
-        hours = String(hoursNumber) + ' час'
-    } else if (hoursNumber > 1 && hoursNumber < 5) {
-        hours = String(hoursNumber) + ' часа'
-    } else if (hoursNumber >= 5) {
-        hours = String(hoursNumber) + ' часов'
-    } else {
-        hours = ''
-    }
-
-    if (minutesNumber % 10 === 1) {
-        minutes = String(minutesNumber) + ' минута'
-    } else if (minutesNumber % 10 > 1 && minutesNumber % 10 < 5) {
-        minutes = String(minutesNumber) + ' минуты'
-    } else if (minutesNumber % 10 >= 5) {
-        minutes = String(minutesNumber) + ' минут'
-    } else {
-        minutes = ''
-    }
 
     return(
         <li className="card" key={key}>
@@ -37,14 +11,9 @@ function PostCard({ key, card}) {
                     {card.name}
                 </p>
                 <p className="card__duration">
-                    {(hours!=='' && minutes!=='') ? hours + ' ' + minutes : hours + minutes}
+                    {card.description}
                 </p>
             </div>
-            <a className="card__link" rel="noreferrer" target="_blank"
-               href={card.trailerLink}>
-                <img className="card__image" alt={card.name}
-                     src={pathName.pathname === "/movies" ? `https://api.nomoreparties.co${card.image.url}` : card.image}/>
-            </a>
         </li>
     )
 }
--- frontend/src/components/posts_landing/posts_card_list/PostsCardsList.js
+++ frontend/src/components/posts_landing/posts_card_list/PostsCardsList.js
@@ -3,24 +3,24 @@ import Preloader from "../preloader/Preloader";
 import "./PostsCardsList.css";
 import React from "react";
 
-function PostsCardList({posts = [], addPosts, listLength, searchDone}) {
+function PostsCardList({posts, addPosts, listLength, searchDone}) {
 
     return (
         <section className="movies">
-            {posts !== null ?
+            {posts.length !== 0 ?
                 <ul className="cards">
                     {posts.map((card) => {
                         return (
-                            <PostCard key={card.id} card={card}/>
+                            <PostCard key={card._id} card={card}/>
                         )
                     }).slice(0, listLength)
                     }
                 </ul>
                 : <></>
             }
-            {posts === null ? (searchDone ? <p className="text">По Вашему запросу ничего не найдено</p>
+            {posts.length === 0 ? (searchDone ? <p className="text">По Вашему запросу ничего не найдено</p>
                     : <p className="text">Введите название объявления в поисковой строке</p>) :
-                /*(posts.length > listLength) &&*/
+                (posts.length > listLength) &&
                 <Preloader addPosts={addPosts}/>}
         </section>
 
