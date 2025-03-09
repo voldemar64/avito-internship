9ffd0248be1135d98c1ed7a7728367b4c27c2130
voldemar <voldemar22864@gmail.com>
2025-03-09 16:16:51 +0300

    feat: main component

--- frontend/src/components/main_landing/main/Main.js
+++ frontend/src/components/main_landing/main/Main.js
@@ -1,11 +1,29 @@
-import "./Main.css";
 import React from "react";
+import AboutProject from "../about_project/AboutProject";
+import Search from "../search/Search";
+import Benefits from "../benefits/Benefits";
+import Sources from "../sources/Sources";
+import Conversation from "../conversation/Conversation";
+import MassMedia from "../mass_media/MassMedia";
+import AboutMe from "../about_me/AboutMe";
+import Cities from "../cities/Cities";
 
-function Main() {
+function Main({ loggedIn, postsTypeFilter, handleSearch }) {
   return (
-    <div className="main_landing">
-      <h1 className="main_landing__title">Главная страница</h1>
-    </div>
+    <>
+      <AboutProject />
+      <Search
+        loggedIn={loggedIn}
+        postsTypeFilter={postsTypeFilter}
+        handleSearch={handleSearch}
+      />
+      <Benefits />
+      <Sources />
+      <Conversation />
+      <MassMedia />
+      <AboutMe />
+      <Cities />
+    </>
   );
 }
 
