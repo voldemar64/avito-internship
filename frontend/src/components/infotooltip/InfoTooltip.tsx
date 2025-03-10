3636273393b20cdf96f8860cc4883b01285f9c7b
Vladimir Kostyuk <voldemar22864@gmail.com>
2025-03-10 17:49:33 +0300

    refactor: rewrote additional pages & components on TypeScript

---
+++ frontend/src/components/infotooltip/InfoTooltip.tsx
@@ -0,0 +1,42 @@
+import "./InfoTooltip.css";
+import React from "react";
+
+// Типизация пропсов компонента
+interface InfoTooltipProps {
+  isOpen: boolean;
+  onClose: () => void;
+  onOverlayClick: (e: React.MouseEvent<Element, MouseEvent>) => void;
+  photo: string;
+  title: string;
+}
+
+const InfoTooltip: React.FC<InfoTooltipProps> = ({ isOpen, onClose, onOverlayClick, photo, title }) => {
+  React.useEffect(() => {
+    if (isOpen) {
+      const timer = setTimeout(() => {
+        onClose();
+      }, 5000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [isOpen, onClose]);
+
+  return (
+      <article
+          className={`popup${isOpen ? ` popup_opened popup_auto-close` : ""}`}
+          onClick={onOverlayClick}
+      >
+        <div className="popup__container">
+          <img src={photo} alt="Картинка" className="popup__photo" />
+          <h2 className="popup__title popup__title_info">{title}</h2>
+          <button
+              type="button"
+              className="popup__close-button"
+              onClick={onClose}
+          />
+        </div>
+      </article>
+  );
+};
+
+export default InfoTooltip;
---
+++ frontend/src/components/not_found/NotFound.tsx
@@ -0,0 +1,24 @@
+import "./NotFound.css";
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+// Типизация компонента
+const NotFound: React.FC = () => {
+    const navigate = useNavigate();
+
+    function goBack() {
+        navigate(-1);
+    }
+
+    return (
+        <section className="not-found">
+            <h1 className="not-found__title">404</h1>
+            <p className="not-found__subtitle">Страница не найдена</p>
+            <Link className="not-found__link" to="#" onClick={goBack}>
+                Назад
+            </Link>
+        </section>
+    );
+};
+
+export default NotFound;
