f052e649e628adf00fbfe6fcf6516de76c651ad7
Vladimir Kostyuk <voldemar22864@gmail.com>
2025-03-10 17:50:51 +0300

    refactor: rewrote posts pages on TypeScript

---
+++ frontend/src/components/posts_landing/posts/Posts.tsx
@@ -0,0 +1,53 @@
+﻿import SearchForm from "../search_form/SearchForm";
+import PostsCardsList from "../posts_card_list/PostsCardsList";
+import React from "react";
+
+interface PostsProps {
+    postsTypeFilter: (type: string) => void;
+    handleSearch: (searchValue: string) => void;
+    posts: { _id: string; name: string; url: string; likes: string[]; owner: string; type: string }[]; // Типизация массива постов
+    addPosts: () => void;
+    onSave: (card: any) => void;
+    onCardClick: (card: any) => void;
+    onCardDelete: (card: any) => void;
+    onEditButtonClick: (card: any) => void;
+    listLength: number;
+    searchDone: boolean;
+    enabled: boolean;
+}
+
+const Posts: React.FC<PostsProps> = ({
+                                         postsTypeFilter,
+                                         handleSearch,
+                                         posts,
+                                         addPosts,
+                                         onSave,
+                                         onCardClick,
+                                         onCardDelete,
+                                         onEditButtonClick,
+                                         listLength,
+                                         searchDone,
+                                         enabled,
+                                     }) => {
+    return (
+        <>
+            <SearchForm
+                postsTypeFilter={postsTypeFilter}
+                handleSearch={handleSearch}
+                enabled={enabled}
+            />
+            <PostsCardsList
+                posts={posts}
+                addPosts={addPosts}
+                onSave={onSave}
+                onCardClick={onCardClick}
+                onCardDelete={onCardDelete}
+                onEditButtonClick={onEditButtonClick}
+                listLength={listLength}
+                searchDone={searchDone}
+            />
+        </>
+    );
+}
+
+export default Posts;
---
+++ frontend/src/components/posts_landing/posts_card_list/PostsCardsList.tsx
@@ -0,0 +1,62 @@
+﻿import PostCard from "../post_card/PostCard";
+import Preloader from "../preloader/Preloader";
+import "./PostsCardsList.css";
+import React from "react";
+
+interface PostsCardsListProps {
+  posts: { _id: string; name: string; url: string; likes: string[]; owner: string; type: string }[]; // Типизация массива постов
+  addPosts: () => void;
+  onSave: (card: any) => void;
+  onCardClick: (card: any) => void;
+  onCardDelete: (card: any) => void;
+  onEditButtonClick: (card: any) => void;
+  listLength: number;
+  searchDone: boolean;
+}
+
+const PostsCardsList: React.FC<PostsCardsListProps> = ({
+                                                         posts,
+                                                         addPosts,
+                                                         onSave,
+                                                         onCardClick,
+                                                         onCardDelete,
+                                                         onEditButtonClick,
+                                                         listLength,
+                                                         searchDone,
+                                                       }) => {
+  return (
+      <section className="posts">
+        {posts.length !== 0 ? (
+            <ul className="cards">
+              {posts
+                  .map((card) => {
+                    return (
+                        <PostCard
+                            key={card._id}
+                            card={card}
+                            onSave={onSave}
+                            onCardClick={onCardClick}
+                            onCardDelete={onCardDelete}
+                            onEditButtonClick={onEditButtonClick}
+                        />
+                    );
+                  })
+                  .slice(0, listLength)}
+            </ul>
+        ) : (
+            <></>
+        )}
+        {posts.length === 0 ? (
+            searchDone ? (
+                <p className="text">По Вашему запросу ничего не найдено</p>
+            ) : (
+                <p className="text">Введите название объявления в поисковой строке</p>
+            )
+        ) : (
+            posts.length > listLength && <Preloader addPosts={addPosts} />
+        )}
+      </section>
+  );
+}
+
+export default PostsCardsList;
