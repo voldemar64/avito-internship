8ad68b407fd6ff5cd4299199fe134d05a20e3f74
Vladimir Kostyuk <voldemar22864@gmail.com>
2025-03-19 19:53:13 +0300

    refactor: eslint config

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -2,11 +2,11 @@ import { fixupConfigRules, fixupPluginRules } from "@eslint/compat";
 import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import prettier from "eslint-plugin-prettier";
-import globals from "globals";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
 import { FlatCompat } from "@eslint/eslintrc";
+import typescript from "@typescript-eslint/eslint-plugin";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -22,25 +22,22 @@ export default [
       "eslint:recommended",
       "plugin:react/recommended",
       "plugin:react-hooks/recommended",
-      "plugin:prettier/recommended"
+      "plugin:prettier/recommended",
+      "plugin:@typescript-eslint/recommended"
     )
   ),
   {
+    parser: "@typescript-eslint/parser",
     plugins: {
       react: fixupPluginRules(react),
       "react-hooks": fixupPluginRules(reactHooks),
       prettier: fixupPluginRules(prettier),
+      "@typescript-eslint": typescript,
     },
 
     languageOptions: {
-      globals: {
-        ...globals.browser,
-        ...globals.node, // Добавляем глобальные переменные Node.js
-      },
-
       ecmaVersion: "latest",
       sourceType: "module",
-
       parserOptions: {
         ecmaFeatures: {
           jsx: true,
@@ -58,6 +55,8 @@ export default [
       "prettier/prettier": "error",
       "react/prop-types": "off",
       "react/react-in-jsx-scope": "off",
+      "@typescript-eslint/no-unused-vars": ["error"],
+      "@typescript-eslint/explicit-module-boundary-types": "off",
     },
   },
 ];
